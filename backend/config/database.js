127f70b5e0560d3628e660f3c8e0d6112f073b1e
ehu096 <eric.yj.hu@cn.pwc.com>
2026-05-12 16:26:50 +0800

    fix: use Railway PG env var names in database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'postgres'),
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'fh6'),
-      user: env('DATABASE_USERNAME', ''),
-      password: env('DATABASE_PASSWORD', ''),
+      host: env('PGHOST', env('DATABASE_HOST', '127.0.0.1')),
+      port: env.int('PGPORT', env.int('DATABASE_PORT', 5432)),
+      database: env('PGDATABASE', env('DATABASE_NAME', 'fh6')),
+      user: env('PGUSER', env('DATABASE_USERNAME', '')),
+      password: env('PGPASSWORD', env('DATABASE_PASSWORD', '')),
       ssl: env.bool('DATABASE_SSL', false),
     },
     pool: { min: 0, max: 10 },
